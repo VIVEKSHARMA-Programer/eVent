da25575f8e7b2c6c2977344c3a9c20626c8669ba
s310416 <vivek9811@yahoo.in>
2019-09-27 23:29:44 +0930

    adding function for hamburger menu.
    Signed-off-by: s310416 <vivek9811@yahoo.in>

---
+++ js/event.js
@@ -0,0 +1,10 @@
+// Function for top navigation //
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
